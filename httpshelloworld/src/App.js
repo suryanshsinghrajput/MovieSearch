3bd64998a1725f473a8078101e7335d3d16c35d5
suryanshsinghrajput <suryanshrajput222@gmail.com>
2022-09-22 12:00:21 +0530

    newwwww

--- httpshelloworld/src/App.js
+++ httpshelloworld/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import axios from "axios";
+import { useState, useEffect } from 'react';
 
 //enguerrand-blanchy-3oADW0Ptj8c-unsplash
-import localImageDiamondMoon from "./images/enguerrand-blanchy-3oADW0Ptj8c-unsplash.jpg";
+import localAntEaterImage from "./images/enguerrand-blanchy-3oADW0Ptj8c-unsplash.jpg";
 let string1 = `hope you are seeing https in your browser address bar`;
 
 let apikeyfromnasa = `JjP84CKefxzmg2fyAvN4zWsRyAAqg1nzrXvHdtc6`;
 let loadApodURI = `https://api.nasa.gov/planetary/apod?api_key=${apikeyfromnasa}`;
+let stringclickButton = `click the button to load data`
+let stringloading = `loading........`;
+let stringloaded = `NASA DATA IS HERE FROM AXIOS`;
 
 function App() {
+
+  const [post, setPost] = useState(null);
+  const [loadmessage,setloadmessage] = useState(stringclickButton);
+
+
+
 
   function callNASAAPI() {
     //call NASA API the moment page loads. 
@@ -16,11 +26,64 @@ function App() {
     axios.get(loadApodURI).then(
         (response) => {
             console.log(response.data);
-            //setPost(response.data);
-            //setloadmessage(stringloaded);
+            setPost(response.data);
+            setloadmessage(stringloaded);
         }
     );
 }
+
+function resetAPI() {
+  setPost(null);
+  setloadmessage(stringclickButton);
+}
+
+
+useEffect ( () => {
+  console.log(`data has been updated`);
+},[]);
+
+const outputWhenNull = 
+(
+  
+  <div className="text-center hero my-5">
+  {/* <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" /> */}
+  <h1 className="mb-4">{string1}</h1> 
+  <h1 className="mb-4">{loadmessage}</h1>   
+  <button className="btn btn-primary" onClick={callNASAAPI}>call API</button>
+  <img src={localAntEaterImage} className="img-fluid" alt="..."></img>
+  
+  </div>
+);
+
+if(post==null) 
+    {
+        return outputWhenNull;
+    }
+
+    const outputnotnull = (
+      <div className="text-center hero my-5">
+      {/* <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" /> */}
+      <h1 className="mb-4">{string1}</h1> 
+      <h1 className="mb-4">{stringloaded}</h1>   
+      {/* <button className="btn btn-primary" onClick={callNASAAPI}>call API</button> */}
+      <button className="btn btn-primary" onClick={resetAPI}>resetAPI</button>
+
+      <p>{post.date}</p>
+      <img src={post.hdurl} className="img-fluid" alt="..."></img>
+      <hr></hr>
+      <img src={localAntEaterImage} className="img-fluid" alt="..."></img>
+      
+      
+      </div>
+    );
+
+    if(post!=null) 
+    {
+     return outputnotnull;
+    }
+
+
+
   return (
     <div className="App">
       <div className="text-center hero my-5">
@@ -29,9 +92,9 @@ function App() {
         <hr></hr>
         <button className="btn btn-primary" onClick={callNASAAPI}>call API</button>
         <hr></hr>
-        <div className='container-fluid'>
+        {/*<div className='container-fluid'>
           <img src={localImageDiamondMoon} className="img-fluid" alt="..."></img>  
-        </div>
+  </div>*/}
       </div>       
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
